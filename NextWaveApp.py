e6fe31158b985e44db2eabeab4c22dd3c683b59f
EugeneW910428 <149223513+EugeneW910428@users.noreply.github.com>
2024-04-27 15:30:11 -0700

    Update NextWaveApp.py

--- NextWaveApp.py
+++ NextWaveApp.py
@@ -16,7 +16,8 @@ nltk.download('stopwords')
 nltk.download('wordnet')
 
 # Streamlit UI setup
-st.title('Welcome to NextWave - your best sentiment analysis tool')
+st.title('Welcome to NextWave')
+st.write('- your best sentiment analysis tool')
 
 # Upload functionality
 #uploaded_file = st.file_uploader("Upload your customer reviews JSONL file", type='jsonl')
