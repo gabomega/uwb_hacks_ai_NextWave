8470bb291623ce8f8d6f8ae218b1a2d298f3273a
EugeneW910428 <149223513+EugeneW910428@users.noreply.github.com>
2024-04-27 15:14:51 -0700

    Update NextWaveApp.py

--- NextWaveApp.py
+++ NextWaveApp.py
@@ -81,7 +81,7 @@ if uploaded_file is not None:
             # Display summary
             st.markdown(f"<h3 style='color:black; font-size:24px;'>Your customer feedback overall is {sentiment_summary}</h3>", unsafe_allow_html=True)
             st.write(f"<h3 style='color:black; font-size:24px;'>Average Rating for {selected_product}: {average_rating:.2f}</h3>", unsafe_allow_html=True)
-            
+            st.write('Here is the Word Cloud summary of your customer experience:')
             # Generate and display WordCloud
             wordcloud = WordCloud(width=800, height=400).generate(' '.join(filtered_data['cleaned_text']))
             plt.figure(figsize=(10, 5))
