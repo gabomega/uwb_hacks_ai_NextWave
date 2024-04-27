7eecf13baca333a98ece8784c6b587d2f9d229e3
EugeneW910428 <149223513+EugeneW910428@users.noreply.github.com>
2024-04-27 11:01:16 -0700

    Create NextWaveApp.py

---
+++ NextWaveApp.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import pandas as pd
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import nltk
+
+# Download NLTK resources
+nltk.download('vader_lexicon')
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+# Function to preprocess text
+
+
+# Function to perform sentiment analysis
+
+
+# Function to generate word cloud for selected product (asin)
+
+
+# Streamlit UI
+st.title('Welcome to NextWave')
+
+# File upload option for the user to upload a file containing product reviews
+
+# Perform sentiment analysis when the user uploads a file
+
+    
+    # Generate word cloud for selected product
+  
+
+
+
+
+
+
