43a87f71f78d2d20962ce77fb70892d99c4012f6
Gabrielle Omega <143249690+gabomega@users.noreply.github.com>
2024-04-27 14:46:24 -0700

    Update NextWaveApp.py

--- NextWaveApp.py
+++ NextWaveApp.py
@@ -70,11 +70,11 @@ if uploaded_file is not None:
                 sentiment_summary = "Negative"
                 sentiment_color = "red"
 
-
             # Display summary
+            sentiment_color = "black"
             st.markdown(f"<h3 style='color:{sentiment_color}; font-size:24px;'>Your customer feedback Summary (Sentiment): {sentiment_summary}</h3>", unsafe_allow_html=True)
             #st.write(f"Your customer feedback overall is {sentiment_summary}")
-            st.write(f"Average Rating for {selected_product} : {average_rating:.2f}")
+            st.write(f"<h3 style='color:{sentiment_color}; font-size:24px;'>Average Rating for {selected_product}: {average_rating:.2f}</h3>", unsafe_allow_html=True)
         else:
             st.write("Product not found. Please try again.")
         # Generate and display WordCloud
