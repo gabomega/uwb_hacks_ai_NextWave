9b78f1815d202038109ab2e35d52326bc5315e9a
EugeneW910428 <149223513+EugeneW910428@users.noreply.github.com>
2024-04-27 21:34:03 -0700

    Update NextWaveApp.py

--- NextWaveApp.py
+++ NextWaveApp.py
@@ -18,10 +18,7 @@ nltk.download('wordnet')
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-col1, col2, col3 = st.columns(3)
-col1.metric("Temperature", "70 °F", "1.2 °F")
-col2.metric("Wind", "9 mph", "-8%")
-col3.metric("Humidity", "86%", "4%")
+
 # Define page functions
 def upload_and_view_results():
     st.title('Welcome to NextWave')
